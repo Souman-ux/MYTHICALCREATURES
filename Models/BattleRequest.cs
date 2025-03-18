7d063800b5835c44629544b0fe07b8ed130e9718
Souman-ux <mughalsomi61@gmail.com>
2025-03-18 12:26:43 +0200

    .Mythical Creatures web Api Project.

---
+++ Models/BattleRequest.cs
@@ -0,0 +1,8 @@
+namespace MythicalCreatures.Models
+{
+    public class BattleRequest
+    {
+        public Creature Creature1 { get; set; }
+        public Creature Creature2 { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+var app = builder.Build();
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+app.UseRouting();
+app.UseAuthorization();
+app.MapControllers();
+
+
+app.Run();
